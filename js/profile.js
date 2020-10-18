62c6916154ad24aa8fe1ac2787475574190c5cf3
Hossam Tarek <hossamtarek250@gmail.com>
2020-10-18 03:47:51 +0200

    Add behavior to the save button.

--- js/profile.js
+++ js/profile.js
@@ -4,16 +4,24 @@ import UserData, {InvalidPassword, InvalidEmail, InvalidPhoneNumber, WrongPasswo
 let name = document.getElementById("name");
 let email = document.getElementById("email");
 let password = document.getElementById("password");
+let newPassword = document.getElementById("new-password");
 let phoneNumber = document.getElementById("phone-number");
 let address = document.getElementById("address");
 let birthday = document.getElementById("birthday");
 let gender = document.getElementById("gender");
 
+// All error messages
+let nameError = document.getElementById("name-validation");
+let emailError = document.getElementById("email-validation");
+let passwordError = document.getElementById("password-validation");
+let phoneNumberError = document.getElementById("phone-number-validation");
+let addressError = document.getElementById("address-validation");
+let birthdayError = document.getElementById("birthday-validation");
+
 // All buttons
 let editButton = document.getElementById("form__edit-button");
 let saveButton = document.getElementById("form__save-button");
 let cancelButton = document.getElementById("form__cancel-button");
-
 
 // Get the user data from the session storage.
 let userData = sessionStorage.getItem("UserData");
@@ -53,12 +61,87 @@ function cancelChanges() {
 
     editButton.disabled = false;
     showData();
+    password.value = "";
+    newPassword.value = "";
     disableInputs(true);
+    hideErrorMessages();
+}
+
+// Hide all error messages
+function hideErrorMessages() {
+    nameError.style.display = "none";
+    emailError.style.display = "none";
+    passwordError.style.display = "none";
+    phoneNumberError.style.display = "none";
+    addressError.style.display = "none";
+    birthdayError.style.display = "none";
 }
 
 // Save all changes
 function saveChanges() {
+    let tempUser = UserData.getCopy(userData);
 
+    if (name.value === "") {
+        nameError.style.display = "block";
+        return;
+    } else {
+        nameError.style.display = "none";
+        tempUser.setName(name.value);
+    }
+
+    try {
+        tempUser.setEmail(email.value);
+        emailError.style.display = "none";
+    } catch (error) {
+        emailError.style.display = "block";
+        return;
+    }
+
+    if (password.value !== "") {
+        try {
+            tempUser.changePassword(newPassword);
+        } catch (error) {
+            passwordError.textContent = error.message;
+            passwordError.style.display = "block";
+            return;
+        }
+    }
+
+    try {
+        tempUser.setPhoneNumber(phoneNumber.value);
+        phoneNumberError.style.display = "none";
+    } catch (error) {
+        phoneNumberError.style.display = "block";
+        return;
+    }
+
+    if (address.value === "") {
+        addressError.style.display = "block";
+        return;
+    } else {
+        addressError.style.display = "none";
+        tempUser.setAddress(address.value);
+    }
+
+    if (Date.parse(birthday.value) > Date.now()) {
+        birthdayError.style.display = "block";
+        return;
+    } else {
+        birthdayError.style.display = "none";
+        tempUser.setBirthday(Date.parse(birthday.value));
+    }
+
+    tempUser.setGender(gender.value);
+
+    userData = tempUser;
+    sessionStorage.setItem("UserData", JSON.stringify(userData.getObject()));
+
+    editButton.disabled  = false;
+    saveButton.style.visibility = "hidden";
+    cancelButton.style.visibility = "hidden";
+    disableInputs(true);
+
+    showData();
 }
 
 // Show data in all the input fields
@@ -79,16 +162,12 @@ function showData() {
     birthday.value = `${year}-${month}-${day}`;
 }
 
-// Save the changed data
-function saveData() {
-
-}
-
 // Change all inputs availability
 function disableInputs(disabled) {
     name.disabled = disabled;
     email.disabled = disabled;
     password.disabled = disabled;
+    newPassword.disabled = disabled;
     phoneNumber.disabled = disabled;
     address.disabled = disabled;
     birthday.disabled = disabled;
