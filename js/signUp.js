d649adc752de07a53c0205a4024e1eb6fb9f3775
MahmoudShalma <love_egypt170@yahoo.com>
2020-10-19 04:34:22 +0200

    Solve conflict2 (#)

--- js/signUp.js
+++ js/signUp.js
@@ -1,60 +1,73 @@
-let check = document.getElementById("check");
-let check1 = document.getElementById("check1");
-let check2 = document.getElementById("check2");
-let check3 = document.getElementById("check3");
+import UserData from "./UserData.js";
 
-let nameInput=document.getElementById("name_input");
+if( window.location.href.indexOf('/signUp.html') !== -1  && sessionStorage.getItem("UserData") !== null ){
+    window.location.href ="./index.html";
+}
+let validateName = document.getElementById("validate-name");
+let validateMail = document.getElementById("validate-email");
+let validatePasword = document.getElementById("validate-password");
+let validateRepeatPassword = document.getElementById("validate-repeat-password");
+
+let nameInput = document.getElementById("name_input");
 let emailInput = document.getElementById("email_input");
 let passwordInput = document.getElementById("password_input");
-let passwordInput2 = document.getElementById("password_input2");
-function validateForm() {
+let passwordRepeatInput = document.getElementById("password-repeat-input");
 
-    if(nameInput.value.trim()==""){
+function createUser() {
+    try {
+        let user = new UserData(nameInput.value,emailInput.value, passwordInput.value);
 
-        check.innerHTML="Please Enter your name.";
-        check.style.color = "red";
-    } else {
-        check.innerHTML="";
-    }
-     if (!validateEmail(emailInput.value)) {
-        check1.innerHTML="Please Enter Valid Email.";
-        check1.style.color = "red";
-    } else {
-        check1.innerHTML="";
-    }
+        validateName.innerHTML = validateMail.innerHTML = validatePasword.innerHTML = "";
 
-    if(passwordInput.value.length < 8) {
-        check2.innerHTML="Password must be 8 chars at least.";
-        check2.style.color = "red";
-    } else {
-        check2.innerHTML="";
-        if(passwordInput.value.length != passwordInput2.value.length ){
-        check3.innerHTML="Password must be same";
-        check3.style.color = "red";
-    }
-        else{
-            check3.innerHTML="";
+        if (passwordInput.value != passwordRepeatInput.value) {
+            validateRepeatPassword.innerHTML = "Password must be same";
+            validateRepeatPassword.style.color = "red";
+        } else {
+            validateRepeatPassword.innerHTML = "";
+
+            if (localStorage.getItem("UserData") === null) { //Handle adding first user (No local storage set before)
+                localStorage.setItem("UserData", '[]');
+            }
+
+            // get old user data
+            let oldUsers = localStorage.getItem("UserData");
+            oldUsers = JSON.parse(oldUsers);
+
+            if (!checkFoundedEmail(oldUsers, emailInput.value)) {
+                oldUsers.push(user.getObject());
+                let newUsers = JSON.stringify(oldUsers);
+                localStorage.setItem("UserData", newUsers);
+                document.querySelector('#signUp-form').reset();
+            }
+            else {
+                validateMail.innerHTML = `This email (${emailInput.value}) is founded in our system`;
+                validateMail.style.color = "red";
+            }
+        }
+    } catch (error) {
+        validateName.innerHTML = validateMail.innerHTML = validatePasword.innerHTML = "";
+        if (error.message === "Invalid name , name mustn't be empty.") {
+            validateName.innerHTML = error.message;
+            validateName.style.color = "red";
+        }
+        else if (error.message === "Invalid email") {
+            validateMail.innerHTML = error.message;
+            validateMail.style.color = "red";
+        }
+        else if (error.message === "Invalid password, password must be 8 characters or more.") {
+            validatePasword.innerHTML = error.message;
+            validatePasword.style.color = "red";
         }
     }
+}
+document.getElementById("signup-btn").addEventListener("click", function () {
+    createUser();
+});
 
-    if(check.innerHTML==""&&check1.innerHTML==""&&check2.innerHTML==""&& check3.innerHTML=="")
-    {
-        alert("well done! Your account has been created successfully" ); 
-        nameInput.value="";
-        emailInput.value="";
-        passwordInput.value="";
-        passwordInput2.value="";
-
+function checkFoundedEmail(oldUsers, newEmail) {
+    for (let index = 0; index < oldUsers.length; index++) {
+        if (oldUsers[index].email == newEmail)
+            return true
     }
-    
+    return false;
 }
-//Valid Email Function 
-function validateEmail(email) 
-{
-    let re = /\S+@\S+\.\S+/;
-    return re.test(email);
-}
-
-document.getElementById("signup-btn").addEventListener("click", function() {
-    validateForm();
-  });
